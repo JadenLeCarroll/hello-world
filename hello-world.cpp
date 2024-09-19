ba7083f85f962fb147c8b7d4c2250564c2034ba7
JadenLeCarroll <jadencarroll2018@gmail.com>
2024-09-18 22:53:15 -0700

    Hello, World!

---
+++ hello-world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!" << endl;
+	// Prints "Hello, World!" to the console 
+
+	return 0;
+	// Indicates that the program has run successfully and exits
+}
